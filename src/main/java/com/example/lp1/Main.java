f0443f92f438cf9b817d096887ad266a47ace04f
Leandro Monteiro <leandrocmonteiro2005@gmail.com>
2026-01-02 17:59:08 +0000

    criação estrutura

--- src/main/java/com/example/lp1/Main.java
+++ src/main/java/com/example/lp1/Main.java
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Main {
 
     public static void main(String[] args) {
-        
+        System.out.println("-----Simulador Dia a Dia-----");
+        System.out.println("1 - Avançar Hora");
+        System.out.println("2 - Inserir utente");
+        System.out.println("3 - Chamar utente");
+        System.out.println("4 - Listar utentes");
+        System.out.println("5 - Listar médicos");
     }
 }
